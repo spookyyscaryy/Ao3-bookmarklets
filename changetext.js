5a748bb63817343de6c1966e1cc1a0c5859d7b39
Mariu <envielestrange@gmail.com>
2021-11-04 10:31:23 +0100

    space

--- changetext.js
+++ changetext.js
@@ -2,9 +2,10 @@ javascript: void
 function() {
 	//gets the text of the selection
     var selezione = window.getSelection();
-    var daCambiare = selezione.toString();
+    var stringaSelezione = selezione.toString();
+    var daCambiare = new RegExp(stringaSelezione, "ig");
     //popup
-    var cambiaIn = prompt("Modify with:", " ");
+    var cambiaIn = prompt("Modify with:", "");
     //changes the text!
     var work = document.getElementById("workskin").innerHTML;
     work = work.replaceAll(daCambiare, cambiaIn);
