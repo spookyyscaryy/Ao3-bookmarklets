24e35ab74d652c7a19998ab3cde9d595141bcc82
Mariu <envielestrange@gmail.com>
2021-11-12 13:33:51 +0100

    .

--- yourname.js
+++ yourname.js
@@ -1,6 +1,6 @@
 javascript:void function(){
-    var yN = new RegExp("Y/N", "ig");
-    var cambiaIn = prompt("Modify with:", " "),
+    let yN = new RegExp("Y/N", "ig");
+    let cambiaIn = prompt("Modify with:", ""),
     work = document.getElementById("workskin").innerHTML;
 work = work.replaceAll(yN, cambiaIn);
 document.getElementById("workskin").innerHTML = work;
