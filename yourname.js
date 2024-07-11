e315405e3fddff6463eb658a2930ba04e1b05655
spookyyscaryy <katelyn-lim@outlook.com>
2024-07-11 14:55:44 +0100

    Update yourname.js

--- yourname.js
+++ yourname.js
@@ -1,5 +1,5 @@
 javascript:void function(){
-    let yN = new RegExp("Y/N", "ig");
+    let Lys = new RegExp("Y/N", "ig");
     let cambiaIn = prompt("Modify with:", ""),
     work = document.getElementById("workskin").innerHTML;
 work = work.replaceAll(yN, cambiaIn);
@@ -10,7 +10,7 @@ document.getElementById("workskin").innerHTML = work;
 
 
 javascript:void function(){
-    let yN = new RegExp(/[\[\(]?Y\/N(?!\/n)[\]\)]?/,"ig");
+    let Lys = new RegExp(/[\[\(]?Y\/N(?!\/n)[\]\)]?/,"ig");
     let cambiaIn = prompt("Modify with:", ""),
     work = document.getElementById("workskin").innerHTML;
 work = work.replaceAll(yN, cambiaIn);
